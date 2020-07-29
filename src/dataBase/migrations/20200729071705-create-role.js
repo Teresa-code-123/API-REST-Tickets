aae19805a43d1cc09c45c1831b30e84bd5e8b3ba
Roxnny A Barrios C <maalex.programer@gmail.com>
2020-07-29 13:36:20 -0400

    migration for user roles

---
+++ src/dataBase/migrations/20200729071705-create-role.js
@@ -0,0 +1,28 @@
+export default {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('Roles', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				allowNull: false,
+				unique: true,
+				type: Sequelize.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		})
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('Roles')
+	},
+}
