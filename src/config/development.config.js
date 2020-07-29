3c583aaba78e47d178358da4680e61d6f6d01966
Roxnny A Barrios C <maalex.programer@gmail.com>
2020-07-29 03:00:31 -0400

    development settings for the sequelize

--- src/config/development.config.js
+++ src/config/development.config.js
@@ -5,4 +5,12 @@ dotenv.config()
 // * CONFIGURATIONS OF DEVELOPMENT
 export default {
 	PORT: process.env.DEV_SERVER_PORT,
+	DB: {
+		username: process.env.DEV_DB_USERNAME,
+		password: process.env.DEV_DB_PASSWORD,
+		database: process.env.DEV_DB_NAME,
+		host: process.env.DEV_DB_HOST,
+		port: process.env.DEV_DB_PORT,
+		dialect: process.env.DEV_DB_DIALECT,
+	},
 }
