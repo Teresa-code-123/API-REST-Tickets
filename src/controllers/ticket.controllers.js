33421d027cead8edc40bfa8df75e9faa507c4d35
Roxnny A Barrios C <maalex.programer@gmail.com>
2020-08-01 23:26:15 -0400

    ticket controller - update

--- src/controllers/ticket.controllers.js
+++ src/controllers/ticket.controllers.js
@@ -1,5 +1,5 @@
 import { validationResult } from 'express-validator'
-import { Ticket } from './../database'
+import { Ticket, User } from './../database'
 import { errorsHelpers } from './../helpers'
 
 export const create = async (req, res) => {
@@ -18,6 +18,36 @@ export const create = async (req, res) => {
 	}
 }
 
+export const update = async (req, res) => {
+	try {
+		validationResult(req).throw()
+
+		const { id } = req.params
+
+		const { name, description } = req.body
+
+		await Ticket.update(
+			{ name, description },
+			{
+				where: { id },
+			}
+		)
+
+		const ticket = await Ticket.findByPk(id, {
+			attributes: {
+				exclude: ['UserId', 'userId'],
+			},
+		})
+
+		res.status(200).json({
+			ticket,
+		})
+	} catch (err) {
+		errorsHelpers.catchErros(err, res)
+	}
+}
+
 export default {
 	create,
+	update,
 }
