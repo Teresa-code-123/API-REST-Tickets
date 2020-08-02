c9e2d78c30ac409b1791171e8f159503c9e2ee9d
Roxnny A Barrios C <maalex.programer@gmail.com>
2020-08-01 22:24:24 -0400

    ticket middleware - checkRelationshipUser

---
+++ src/routes/middlewares/ticket.middlewares.js
@@ -0,0 +1,29 @@
+import { Ticket } from './../../dataBase'
+import { errorsHelpers } from './../../helpers'
+
+/**
+ * * checkRelationshipUser
+ * - Verify that the ticket is not assigned to a user in order to be updated
+ */
+export const checkRelationshipUser = async (req, res, next) => {
+	try {
+		const { id } = req.params
+
+		const ticket = await Ticket.findByPk(id)
+
+		if (ticket.userId !== null) {
+			return res.status(400).json({
+				message:
+					'this ticket is already assigned to a user and cannot be updated',
+			})
+		}
+
+		next()
+	} catch (err) {
+		errorsHelpers.catchErros(err, res)
+	}
+}
+
+export default {
+	checkRelationshipUser,
+}
