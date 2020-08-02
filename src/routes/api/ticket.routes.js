83b002f315cc7b734071da75ebdc0426fa6b04ce
Roxnny A Barrios C <maalex.programer@gmail.com>
2020-08-01 23:27:03 -0400

    ticket routes - update

--- src/routes/api/ticket.routes.js
+++ src/routes/api/ticket.routes.js
@@ -3,6 +3,7 @@ import { check } from 'express-validator'
 
 import { validatorHelpers } from './../../helpers'
 import { ticketController } from './../../controllers'
+import { ticketMiddleware } from './../middlewares'
 
 const router = express.Router()
 
@@ -25,6 +26,28 @@ router.post(
 			.withMessage('the description is required'),
 	],
 	ticketController.create
+)
+
+// * THIS ROUTE UPDATES A TICKET IF AND ONLY IF IT IS NOT ASSIGNED TO A USER
+router.put(
+	'/update/:id',
+	[
+		ticketMiddleware.checkRelationshipUser,
+		check('name')
+			.trim()
+			.escape()
+			.notEmpty()
+			.withMessage('the name is required')
+			.custom((value, { req }) => {
+				return validatorHelpers.verifyNameTicket(value, req.params.id)
+			}),
+		check('description')
+			.trim()
+			.escape()
+			.notEmpty()
+			.withMessage('the description is required'),
+	],
+	ticketController.update
 )
 
 export default router
